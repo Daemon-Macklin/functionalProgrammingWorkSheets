af320beb19224381347ff9547791845f8463043c
Daemon-Macklin <daemon.macklin@outlook.com>
2020-02-05 00:46:37 +0000

    Adding more list exercises

--- lab03/lab03List.hs
+++ lab03/lab03List.hs
@@ -1,3 +1,4 @@
+import Data.Char
 --- [1..6]
 
 --- [x*10 | x <- [1..6]]
@@ -16,3 +17,29 @@
 
 isEven :: Integer -> Bool 
 isEven n = (n `mod` 2 == 0)
+
+
+doubleAll :: [Integer] -> [Integer]
+doubleAll xs = [x*2 | x <- xs]
+
+
+capitalise :: String -> String
+capitalise str = [toUpper x | x <- str]
+
+sigma ::Int
+sigma = sum [x^2 | x <- [1..100]]
+
+sigma' :: Int -> Int
+sigma' x = sum [x^2 | x <- [1..x]]
+
+matches:: Integer -> [Integer] -> [Integer]
+matches x xs = [ x | (x',i) <- zip xs [0..], x==x' ]
+
+grid :: Int -> Int -> [(Int, Int)]
+grid x y = [(x,y) | x <- [0..x], y <- [0..y]]
+
+square :: Int -> [(Int, Int)]
+square x = concat [[(x,y) | x <- [0..x], y <- [0..x], x /= y], [(y,x) | x <- [0..x], y <- [0..x], x /= y]]
+
+--myReplicate :: Int -> a -> [a]
+--myReplicate int a = [x | x <- ]
